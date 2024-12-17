c8e61e593dc5b2ebd3d513d546a091ed4e6420ef
VrbaPetr <vrba.workmail@gmail.com>
2024-12-17 20:15:55 +0100

    add api endpoints for elasticsearch

--- services/api/index.js
+++ services/api/index.js
@@ -1,3 +1,4 @@
+// Dependencies
 const express = require('express');
 const { Pool } = require('pg');
 require('dotenv').config();
@@ -5,6 +6,7 @@ require('dotenv').config();
 const app = express();
 const port = 3000;
 
+// DB Connection Pool
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
@@ -16,11 +18,17 @@ const pool = new Pool({
 const { Client } = require('@elastic/elasticsearch');
 const esClient = new Client({ node: 'http://elasticsearch:9200' });
 
+
+
+/**
+ * ENDPOINT
+ * Vyhledavani dat pomoci ElasticSearch
+ */
 app.get('/search', async (req, res) => {
     const query = req.query.q || '';
     try {
         const result = await esClient.search({
-            index: 'example',
+            index: 'airports',
             query: {
                 match: { name: query }
             }
@@ -31,19 +39,69 @@ app.get('/search', async (req, res) => {
     }
 });
 
+/**
+ * ENDPOINT
+ * Indexace dat z PostreSQL do Elasticsearch
+ */
+app.post('/elastic-index', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM airports'); // Získání dat z PostgreSQL
+        const data = result.rows;
+
+        // Indexování každého řádku do Elasticsearch
+        for (const item of data) {
+            try {
+                await esClient.index({
+                    index: 'airports', // Název indexu v Elasticsearch
+                    document: item,    // Data, která se indexují
+                });
+            } catch (err) {
+                console.error('Chyba při indexování dokumentu:', err);
+            }
+        }
+
+        res.send('Data byla indexována do Elasticsearch.');
+    } catch (err) {
+        console.error('Chyba při získávání dat z PostgreSQL:', err);
+        res.status(500).send('Chyba při indexování dat do Elasticsearch.');
+    }
+});
+
+/**
+ * ENDPOINT
+ * Testovaci endpoint
+ */
 app.get('/', (req, res) => {
     res.send('API is running!');
 });
 
+/**
+ * Zobrazeni kompletnich dat z PostgreSQL
+ */
 app.get('/data', async (req, res) => {
     try {
-        const result = await pool.query('SELECT * FROM example');
+        const result = await pool.query('SELECT * FROM airports');
         res.json(result.rows);
     } catch (err) {
         res.status(500).send(err.message);
     }
 });
 
+/**
+ * ENDPOINT
+ * Jmenny seznam letist v databazi
+ */
+app.get('/airports', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT name FROM airports');
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+// Server Init
 app.listen(port, () => {
     console.log(`API running on http://localhost:${port}`);
 });
+
